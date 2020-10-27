ce472146478f233f2ccfedecc132608c3dff885b
Alie Stone <helloaliestone@gmail.com>
2020-10-27 15:31:41 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,15 +3,15 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 get '/name' do
-  "My name is"
+  "My name is Alie"
 end
 
 get '/hometown' do
-  "My hometown is"
+  "My hometown is Los Angeles"
 end
 
 get '/favorite-song' do
-  "My favorite song is"
+  "My favorite song is fields of gold"
 end
 
 end 
